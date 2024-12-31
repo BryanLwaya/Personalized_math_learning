ac90d88b65f7da57d0456767411e10c466779746
BryanLwaya <98429501+BryanLwaya@users.noreply.github.com>
2024-12-31 03:14:58 +0300

    created homepage

---
+++ student-app/src/pages/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from '../../components/Hero/Hero'
+import Services from '../../components/Services/Services'
+import Banner from '../../components/Banner/Banner'
+import Banner2 from '../../components/Banner/Banner2'
+import Footer from '../../components/Footer/Footer'
+import Navbar from '../../components/NavBar/NavBar'
+
+const Home = () => {
+    return (
+        <main className='overflow-x-hidden bg-white text-dark'>
+            <Navbar />
+            <Hero />
+            <Services />
+            <Banner />
+            <Banner2 />
+            <Footer />
+        </main>
+    )
+}
+
+export default Home
